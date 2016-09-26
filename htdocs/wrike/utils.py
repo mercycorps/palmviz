faacf949aecd788f6d3f4d2197d1e63adf3fcd10
Mahmood Khan <mkhan@mercycorps.org>
2016-09-26 10:53:13 -0700

    closes #4 pull folders data from wrike

--- htdocs/wrike/utils.py
+++ htdocs/wrike/utils.py
@@ -12,7 +12,7 @@ from django.utils.encoding import smart_text
 
 from django.contrib.auth.models import User
 
-from .models import WrikeOauth2Credentials, CustomField, Contact
+from .models import WrikeOauth2Credentials, CustomField, Contact, Folder
 
 logger = logging.getLogger(__name__)
 
@@ -109,4 +109,34 @@ def process_wrike_contacts():
             logger.error(e)
             return False
     return True
+
+
+def process_wrike_folders():
+    """
+    Fetches Folder(Projects) from Wrike's Mercy Corps account and stores them.
+    """
+    try:
+        access_token = get_wrike_access_token()
+        headers = {"Authorization": "bearer %s" % access_token}
+        folders = requests.get(settings.WRIKE_FOLDER_API_URL, headers=headers)
+        folders_json = json.loads(folders.text)
+    except Exception as e:
+        logger.error(e)
+        return False
+
+    db_col_names = get_model_fields_names('Folder')
+    data = folders_json['data']
+    for row in data:
+        db_row = {}
+        for col,val in row.iteritems():
+            if col in db_col_names: db_row[col] = smart_text(val)
+        try:
+            field, created = Folder.objects.update_or_create(id=row['id'], defaults=db_row)
+        except Exception as e:
+            logger.error(e)
+            return False
+    return True
+
+
+
 
