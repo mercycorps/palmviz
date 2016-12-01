a0deb8774035ec5a09c87ba8c5d3a88863b9e700
Mahmood Khan <mkhan@mercycorps.org>
2016-12-01 12:38:23 -0800

    added command to automate pulling data from wrike

---
+++ htdocs/wrike/management/commands/get_palm_wrike_data.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from wrike import utils
+
+class Command(BaseCommand):
+    """
+    Usage: python manage.py get_palm_wrike_data
+    """
+    help = 'Fetches PALM Wrike data under "PALM Support" folder'
+
+    def add_arguments(self, parser):
+        #parser.add_argument("-u", "--username", type=str, required=True)
+        #parser.add_argument('--read_ids', nargs='*', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        if utils.process_wrike_data() == False
+            #send out an email
+            pass
