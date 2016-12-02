ddb62eedbb7d5f1debd85633b631d29086241354
Mahmood Khan <mkhan@mercycorps.org>
2016-12-02 07:54:26 -0800

    added 404handler and 500handler

--- htdocs/palmviz/views.py
+++ htdocs/palmviz/views.py
@@ -19,3 +19,21 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['home'] = 'active_tab'
         return context
+
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
